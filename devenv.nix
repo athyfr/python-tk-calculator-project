4f6fe95dd18c6b5e651f1b5bf1193b69d431d9df
athyfr <athyfr@proton.me>
2026-04-07 09:16:41 -0700

    Disable Devenv LSP setup.

--- devenv.nix
+++ devenv.nix
@@ -9,6 +9,7 @@
   languages.python = {
     enable = true;
     package = pkgs.python314.withPackages (ps: [ ps.tkinter ]);
+    lsp.enable = false; # Devenv's LSP setup system doesn't meet my requirements.
     venv.enable = true;
     venv.requirements = ./requirements.txt;
   };
